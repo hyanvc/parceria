87a442edfd62e35368a6284cadaf52f848b4530b
hyancapef <hyan.gomes@capef.com.br>
2024-04-22 14:42:59 -0300

    adicionado os 2 scripts de homolog parceria

---
+++ capefparceriapremiadahmv12.js
@@ -0,0 +1,247 @@
+async function finalizar(cpf, linkunico) {
+    await setupTokenParceria();
+    if (await checkLINKUNICO(cpf, linkunico)) {
+        let eventoValor;
+        var eventoid = document.getElementById("Evento-Parceria");
+        var evento = eventoid.value;
+
+        if (evento.includes("Notícias")) {
+            eventoValor = 19;
+        } else if (evento.includes("Mercado em foco")) {
+            eventoValor = 20;
+        } else if (evento.includes("TV Capef")) {
+            eventoValor = 18;
+        }
+        if (eventoValor) {
+
+            if (await validarPontuar(cpf)) {
+
+                await pontuarCPF(cpf, eventoValor, linkunico);
+            }
+        }
+    }
+
+}
+
+async function validarPontuar(cpf) {
+    return await checkCPFParceria(cpf);
+
+}
+
+// const urlAPI = "https://apiparceriapremiada.capef.com.br"; //producao
+const urlAPI = "https://ici002.capef.com.br/apiparceriapremiada"; //homolog
+const authUserName = "Hero99";
+const authPassword = "d7OwsEqTXc";
+
+async function setupTokenParceria() {
+    const authResponse = await fetch(`${urlAPI}/auth/access-token`, {
+        method: "POST",
+        body: JSON.stringify({
+            username: authUserName,
+            password: authPassword
+        }),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    });
+
+    if (!authResponse.ok) {
+        throw new Error("Failed to obtain authentication token");
+    }
+
+    const authData = await authResponse.json();
+    token = authData.access_Token;
+    localStorage.setItem('authTokenparceria', token);
+}
+
+async function authFetchParceria(url, options = {}) {
+    try {
+        let token = localStorage.getItem('authTokenparceria');
+        const headers = {
+            ...options.headers,
+            "Authorization": `Bearer ${token}`,
+            "Content-Type": "application/json"
+        };
+        const dataResponse = await fetch(url, {
+            ...options,
+            headers
+        });
+
+        if (dataResponse.status === 401) {
+            localStorage.removeItem("authTokenparceria");
+            await setupTokenParceria();
+        }
+
+        if (dataResponse.status === 400) {
+            const result = await dataResponse.json();
+
+            return {
+                status: dataResponse.status,
+                data: result[0]
+            }
+        }
+        if (dataResponse.status === 204) {
+            return {
+                status: dataResponse.status
+            }
+        }
+        if (!dataResponse.ok) {
+            if (dataResponse.status === 415) {
+
+            } else if (await dataResponse.json()) {
+                const result = await dataResponse.json();
+                return {
+                    error: result[0],
+                    status: dataResponse.status
+                }
+            } else {
+                return {
+                    status: dataResponse.status
+                }
+            }
+        } else {
+            const data = await dataResponse.json();
+            return data;
+        }
+
+    } catch (error) {
+        return error
+    }
+}
+
+const apiparceria = authFetchParceria;
+
+async function checkCPFParceria(cpf) {
+    const response = await apiparceria(`${urlAPI}/CPF/${cpf}`);
+    const data = await response;
+
+    if (data.id === "NaoEParticipante") {
+        $('.w-form-done').toggleClass('w-form-done w-form-fail').text("CPF não habilitado para o programa.");
+        return false;
+    }
+
+    const valido = data.id !== null && data.id !== undefined;
+    if (!valido) {
+        $('.w-form-done').toggleClass('w-form-done w-form-fail').text("CPF não cadastrado no Programa.");
+    }
+    return valido;
+}
+
+
+async function checkLINKUNICO(cpf, linkunico) {
+    const body = {
+        "CPF": cpf,
+        "linkunico": linkunico
+    }
+       debugger;
+    const response = await apiparceria(`${urlAPI}/CPF/verificarlink`, {
+        method: "POST",
+        body: JSON.stringify(body)
+    });
+    const data = await response;
+    if (data.status == 400) {
+        $('.w-form-done').toggleClass('w-form-done w-form-fail').text("Você já pontuou neste conteúdo.");
+
+    } else {
+        return data.id !== null && data.id !== undefined;
+    }
+}
+
+async function pontuarCPF(cpf, evento, linkunico) {
+    const pontosJson = {
+        "CPF": cpf,
+        "EventoId": evento,
+        "linkunico": linkunico
+    }
+    const response = await apiparceria(`${urlAPI}/Pontos/com-link-unico`, {
+        method: "POST",
+        body: JSON.stringify(pontosJson)
+    });
+
+    if (response.chamadoPontuacaoId) {
+        $('.w-form-done').text("Seus pontos foram creditados com sucesso!");
+    } else {
+        console.error("Erro ao adicionar pontuação:", response.statusText);
+    }
+}
+
+$(document).ready(function () {
+    var eventoid = document.getElementById("Evento-Parceria");
+    let iddobotao;
+    let contemcodigo = false;
+    var evento = eventoid.value;
+    if (evento.includes("Notícias")) {
+        iddobotao = "btnparceria-noticias";
+    } else if (evento.includes("Mercado em foco")) {
+        iddobotao = "btnparceria-mef";
+    } else if (evento.includes("TV Capef")) {
+        iddobotao = "btnparceria-tvcapef";
+    }
+
+    $('#' + iddobotao).click(function () {
+
+        let idcodigo;
+        var evento = eventoid.value;
+        if (evento.includes("Notícias")) {
+            idcodigo = "-noticias";
+        } else if (evento.includes("Mercado em foco")) {
+            idcodigo = "-mef";
+        } else if (evento.includes("TV Capef")) {
+            idcodigo = "-tvcapef";
+        }
+
+        var iddocodigo = "C-digo-do-conte-do-3" + idcodigo;
+        var inputElement = $('#' + iddocodigo);
+        var inputCpf = $('#cpf-parceria' + idcodigo);
+       debugger;
+        var valorCpf = inputCpf.val();
+        var cpf = valorCpf.replace(/\D/g, '');
+        if (inputElement.length > 0) {
+            var codigodigitado = inputElement.val();
+        }
+        let respostacodigo;
+        if(!idcodigo.includes("noticia"))
+            {
+             respostacodigo = $('#codigopontuacao' + idcodigo).val();
+             contemcodigo = true;
+            } else
+            
+            {
+                respostacodigo = 0;
+            }
+
+        var codigo = respostacodigo;
+        const linkunico = window.location.href;
+
+        if(contemcodigo)
+        {
+            if(codigo != codigodigitado)
+            {
+                $('.w-form-done').toggleClass('w-form-done w-form-fail').text("Código inválido para o vídeo.");
+                return;
+
+            }
+        }
+
+
+        if (codigodigitado != null && codigodigitado != "") {
+            if (codigodigitado != null && codigodigitado != "") {
+                if (codigo === codigodigitado) {
+                    finalizar(cpf, linkunico);
+                }
+            } else { }
+        } else {
+            if(!idcodigo.includes("noticia"))
+            {
+                if (inputElement.length > 0) {
+                    finalizar(cpf, linkunico);
+                }
+            } else
+            {
+                finalizar(cpf, linkunico);
+
+            }
+           
+        }
+    });
+});
